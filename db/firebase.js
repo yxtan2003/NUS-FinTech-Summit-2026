fb3082d3e2bd3fd49107dc80607e923198bfeddb
Yi Xun <yxtan2003@gmail.com>
2026-01-08 16:48:56 +0800

    now

--- db/firebase.js
+++ db/firebase.js
@@ -1,10 +1,10 @@
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-var serviceAccount = require("../firebase-key.json");
+const serviceAccount = require("../firebase-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-var db = admin.firestore();
-module.exports = db;
+const db = admin.firestore();
+module.exports = { db, admin };
